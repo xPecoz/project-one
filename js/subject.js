4c98ec6ccb3f210056fa02dc4adc0cab38692536
AbdulrahmanSallam <119451552+AbdulrahmanSallam@users.noreply.github.com>
2023-02-02 12:18:48 +0200

    Add files via upload

--- js/subject.js
+++ js/subject.js
@@ -252,18 +252,53 @@ inputPass.oninput = () => {
       inputPass.remove();
     }
   }
-};
+  if (inputPass.value[0] == "4") {
+    four.style.display = "block";
+    fourBox.style.display = "grid";
 
-window.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-});
+    five.style.display = "none";
+    fiveBox.style.display = "none";
 
-document.onkeydown = (e) => {
-  let num = 0;
-  let ctrl;
-  e.ctrlKey !== false ? (ctrl = true) : "";
-  e.keyCode == 85 ? (num = 85) : "";
-  if (num == 85 && ctrl == true) {
-    e.preventDefault();
+    six.style.display = "none";
+    sixBox.style.display = "none";
+  } else if (inputPass.value[0] == "5") {
+    four.style.display = "none";
+    fourBox.style.display = "none";
+
+    five.style.display = "block";
+    fiveBox.style.display = "grid";
+
+    six.style.display = "none";
+    sixBox.style.display = "none";
+  } else if (inputPass.value[0] == "6") {
+    four.style.display = "none";
+    fourBox.style.display = "none";
+
+    five.style.display = "none";
+    fiveBox.style.display = "none";
+
+    six.style.display = "block";
+    sixBox.style.display = "grid";
   }
 };
+
+let four = document.querySelector(".four");
+let fourBox = document.querySelector(".four_box");
+let five = document.querySelector(".five");
+let fiveBox = document.querySelector(".five_box");
+let six = document.querySelector(".six");
+let sixBox = document.querySelector(".six_box");
+
+// window.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+// });
+
+// document.onkeydown = (e) => {
+//   let num = 0;
+//   let ctrl;
+//   e.ctrlKey !== false ? (ctrl = true) : "";
+//   e.keyCode == 85 ? (num = 85) : "";
+//   if (num == 85 && ctrl == true) {
+//     e.preventDefault();
+//   }
+// };
